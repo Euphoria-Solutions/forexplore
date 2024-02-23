450355026e876ecfd7fc0c471553a9edb8def05a
Anand-Ochir <anadaochir@gmail.com>
2024-02-23 11:31:46 +0800

    feat(front-end): authentication

--- projects/back/next.config.js
+++ projects/back/next.config.js
@@ -2,6 +2,8 @@ const nextConfig = {
   env: {
     MONGODB_URL_PROD: process.env.MONGODB_URL_PROD || '',
     MONGODB_URL_DEV: process.env.MONGODB_URL_DEV || '',
+    NODEMAILER_EMAIL: process.env.NODEMAILER_EMAIL || '',
+    NODEMAILER_PASS: process.env.NODEMAILER_PASS || '',
     CURRENT_ENV: process.env.CURRENT_ENV || '',
   },
 };
---
+++ projects/back/src/resolvers/otp.ts
@@ -0,0 +1,128 @@
+import jwt from 'jsonwebtoken';
+import nodemailer from 'nodemailer';
+import {
+  MutationCheckOtpArgs,
+  MutationCheckOtpForForgetPassArgs,
+  MutationSendOtpArgs,
+  MutationSendOtpForForgetPassArgs,
+  ResolversParentTypes,
+} from '../generated/generated';
+import { UserModel } from '../models';
+import { otpGenerator } from '../helper';
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.NODEMAILER_EMAIL,
+    pass: process.env.NODEMAILER_PASS,
+  },
+});
+
+const secretKey = 'f0r3xpl0r3';
+export const checkOTP = async (
+  _: ResolversParentTypes,
+  params: MutationCheckOtpArgs
+) => {
+  try {
+    const user = await UserModel.findOne({ _id: params._id });
+
+    const token = jwt.verify(user.otp, secretKey);
+
+    if (typeof token == 'string') return null;
+
+    if (token.otp == params.otp) {
+      console.log('a');
+      const sessionToken = jwt.sign(
+        {
+          _id: user._id,
+          username: user.username,
+        },
+        secretKey,
+        {
+          expiresIn: '7d',
+        }
+      );
+      await UserModel.findByIdAndUpdate(params._id, { emailVerified: true });
+
+      return sessionToken;
+    }
+
+    return null;
+  } catch (err) {
+    throw new Error((err as Error).message);
+  }
+};
+
+export const sendOTP = async (
+  _: ResolversParentTypes,
+  params: MutationSendOtpArgs
+) => {
+  try {
+    const user = await UserModel.findById(params._id);
+    const otp = otpGenerator();
+    const hashedOTP = jwt.sign({ otp }, secretKey, { expiresIn: 300 });
+
+    await UserModel.findByIdAndUpdate(params._id, { otp: hashedOTP });
+
+    await transporter.sendMail({
+      from: 'anadaochir@gmail.com',
+      to: user.email,
+      subject: user.type,
+      text: `
+        verification number: ${otp}
+        `,
+    });
+
+    return true;
+  } catch (err) {
+    throw new Error((err as Error).message);
+  }
+};
+
+export const sendOTPForForgetPass = async (
+  _: ResolversParentTypes,
+  params: MutationSendOtpForForgetPassArgs
+) => {
+  const otp = otpGenerator();
+  const hashedOTP = jwt.sign({ otp }, secretKey, { expiresIn: 300 });
+  try {
+    await UserModel.findOneAndUpdate(
+      { email: params.email },
+      { otp: hashedOTP }
+    );
+
+    await transporter.sendMail({
+      from: 'anadaochir@gmail.com',
+      to: params.email,
+      subject: 'Password Recovery',
+      text: `
+        verification number: ${otp}
+        `,
+    });
+
+    return true;
+  } catch (err) {
+    throw new Error((err as Error).message);
+  }
+};
+
+export const checkOTPForForgetPass = async (
+  _: ResolversParentTypes,
+  params: MutationCheckOtpForForgetPassArgs
+) => {
+  try {
+    const user = await UserModel.findOne({ email: params.email });
+
+    const token = jwt.verify(user.otp, secretKey);
+
+    if (typeof token == 'string') return null;
+
+    if (token.otp == params.otp) {
+      return true;
+    }
+  } catch (err) {
+    throw new Error((err as Error).message);
+  }
+};
